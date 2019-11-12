69895e3c0f806201d142f4db0054d4211c015da2
Garciaj007 <jurielgarcia2010@gmail.com>
2019-11-11 23:15:26 -0500

    Video Player Menu Style Integration

---
+++ VR-Journalism-Project/Assets/Scripts/FocusButtonBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class FocusButtonBehaviour: MonoBehaviour
+{
+    [SerializeField] private MeshRenderer buttonIcon;
+    [SerializeField] private Texture normalTexture;
+    [SerializeField] private Texture hoverTexture;
+
+    public Texture NormalTexture { set => normalTexture = value; }
+    public Texture HoverTexture { set => hoverTexture = value; }
+    private bool HasTriggered { get; set; }
+
+    public void Start()
+    {
+        if (normalTexture != null)
+            buttonIcon.material.mainTexture = normalTexture;
+        else
+            normalTexture = buttonIcon.material.mainTexture;
+    }
+
+    public void OnFocusEnter()
+    {
+        buttonIcon.material.mainTexture = hoverTexture;
+    }
+
+    public void OnFocusExit()
+    {
+        buttonIcon.material.mainTexture = normalTexture;
+    }
+}
