a34ff5180e2fdf0a525b977125017cc9ded092f7
NickMelo19 <44447684+NickMelo19@users.noreply.github.com>
2019-11-13 17:24:47 -0500

    Adding Audio

---
+++ VR-Journalism-Project/Assets/Audio/AudioScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+
+    //public AudioSource[] rSounds;
+
+    //public AudioSource rSound1;
+    //public AudioSource rSound2;
+    //public AudioSource rSound3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // rSounds[Random.Range(0, rSounds.Length)].Play();
+    }
+}
